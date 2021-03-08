9a5f50d61f6359d6586c3ee837617d3423dfad95
gabsgc <gabriellagarccia@gmail.com>
2021-03-08 14:57:06 -0300

    up styles, app and 404

--- app.py
+++ app.py
@@ -42,6 +42,9 @@ def sobre():
 def sopro():
     return render_template('sopro.html')
 
+@app.errorhandler(404)
+def page_not_found(e):   
+    return render_template('404.html'), 404
 
 if __name__ == '__main__':
     app.run(debug=True)
